1f50438935d909738867b9ccd62a856756c66349
JeffReeves <jeff@binary.run>
2020-12-29 15:49:03 -0700

    Start Sequelize demo
    Alpha stage scaffolding tags

--- index.js
+++ index.js
@@ -5,33 +5,52 @@ author: Jeff Reeves
 
 // include config
 const config = require('./config.json');
+const PREFIX = config.command.prefix;
 
-// include Discord library
+// include Sequelize and Discord
+const Sequelize = require('sequelize');
 const Discord = require('discord.js');
 
 // initialize a new client connection
 const client = new Discord.Client();
 
-// once connection is ready
+// [alpha]
+// [beta]
+
+// trigger when bot is ready
 client.once('ready', () => {
 	console.debug('[DEBUG] Bot Ready!');
+	// [gamma]
+});
+
+// on message received
+client.on('message', async message => {
+
+	// log entire message to console
+	console.log('[MESSAGE]', message.content);
+
+	if (message.content.startsWith(PREFIX)) {
+		const input = message.content.slice(PREFIX.length).trim().split(' ');
+		const command = input.shift();
+		const commandArgs = input.join(' ');
+
+		if (command === 'addtag') {
+			// [delta]
+		} else if (command === 'tag') {
+			// [epsilon]
+		} else if (command === 'edittag') {
+			// [zeta]
+		} else if (command === 'taginfo') {
+			// [theta]
+		} else if (command === 'showtags') {
+			// [lambda]
+		} else if (command === 'removetag') {
+			// [mu]
+		}
+	}
 });
 
 // login with OAuth2 token
 let token = process.env.token || config.token;
 client.login(token);
 console.debug('[DEBUG] Bot logged in using token');
-
-// listen for messages
-client.on('message', message => {
-
-	// log entire message to console
-	console.log('[MESSAGE]', message.content);
-
-	// if it contains the the word "ping"
-	if (message.content === '!ping') {
-
-		// send back "Pong." to the channel the message was sent in
-		message.channel.send('Pong.');
-	}
-});
