58b90d6bbf8f9b962b9ee39620c58569b40785a3
JeffReeves <jeff@binary.run>
2021-01-03 05:44:19 -0700

    Add newline to attachment size

--- index.js
+++ index.js
@@ -77,9 +77,6 @@ client.on('message', async message => {
 
 //==[SHARE -> ARCHIVE]=================================================================================================
 
-    // TODO:
-    // - Handle copying of attachments in share channel over to archive channel
-
     // if the channel contains 'share' in the name
     const shareSuffix   = '-share';
     const archiveSuffix = '-archive';
@@ -139,10 +136,8 @@ client.on('message', async message => {
             const archiveEmbed = new Discord.MessageEmbed()
                 .setColor(randomColor)
                 .setTitle(abbreviatedTitle)
-                //.setURL(URL)
-                //.setAuthor(author, authorAvatar, URL)
+                //.setAuthor(author, authorAvatar, authorURL)
                 .setDescription(content)
-                //.setThumbnail('https://i.imgur.com/wSTFkRM.png')
                 .addFields({ 
                         name: '\u200B', 
                         value: `[Original Post](${authorURL})` 
@@ -177,7 +172,7 @@ client.on('message', async message => {
                         // add a link to each file
                         archiveEmbed.addFields({
                             name: 'Attachment', 
-                            value: `[${attachment.name}](${attachment.url}) \`${fileSize}\``, 
+                            value: `[${attachment.name}](${attachment.url})\n\`${fileSize}\``, 
                             inline: true
                         });
                     }
@@ -194,8 +189,10 @@ client.on('message', async message => {
 
             // create additional embeds for any/all URLs in message content
             if(URLs){
+
                 // get total number of URLs
                 const numURLs = URLs.length;
+
                 // send each URL as a separate post
                 URLs.forEach((URL, index) => {
                     archive.send(`\`[URL ${index + 1}/${numURLs}]\` ${URL}`);
