9c791c9c3ea2f80a804aefcc5426df40f248a642
JeffReeves <jeff@binary.run>
2021-01-09 11:36:33 -0700

    Fix prefix undefined

--- commands/archive.js
+++ commands/archive.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const config  = require('../config.json');
+const prefix  = config.command.prefix;
 const archive = config.command.archive;
 
 module.exports = {
